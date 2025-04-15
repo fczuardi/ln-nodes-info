fb4a36b67612c291280fbf65c8a018ed10785f39
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-15 17:10:00 -0300

    feat: lightning nodes listing markup
    The HTML structure of a list of nodes, that is populated by page
    properties.

---
+++ src/routes/nodes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { PageProps } from './$types';
+  let { data }: PageProps = $props();
+  const { nodes } = data;
+</script>
+
+<article>
+  <header id="list-header">
+    <h1>Top Lightning Network Nodes</h1>
+    <p>Ordered by Connectivity <sup><a href="#fn1" aria-describedby="fn1">1</a></sup></p>
+  </header>
+  <ol>
+    {#each nodes as node}
+      <li>
+        <dd>{node.alias}</dd>
+        <dt>{node.channels}</dt>
+      </li>
+    {/each}
+  </ol>
+  <footer>
+    <div id="footnotes">
+      <ol>
+        <li id="fn1">
+          source: <a href="https://mempool.space/lightning/nodes/rankings/connectivity"
+            >mempool.space</a
+          ><a href="#list-header" aria-label="Back to reference">↩</a>
+        </li>
+      </ol>
+    </div>
+  </footer>
+</article>
