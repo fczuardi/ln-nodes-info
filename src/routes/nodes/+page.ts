5d417004cb831da548d4464868e5cdc9b5bcdd9c
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-16 19:20:11 -0300

    feat: error handling for the list fetch
    This patch handles a set of 3 possible errors from a http GET fetch:
      - failed to fetch
      - response not ok
      - response ok, but body not parseable json

--- src/routes/nodes/+page.ts
+++ src/routes/nodes/+page.ts
@@ -1,14 +1,61 @@
 import type { PageLoad } from './$types';
-import { error } from '@sveltejs/kit';
 import {
   PUBLIC_MEMPOOLSPACE_API_URL,
   PUBLIC_MEMPOOLSPACE_ENDPOINT_LN_RANKING_CONNECTIVITY,
 } from '$env/static/public';
 
-export const load: PageLoad = async ({ fetch }) => {
-  const url = `${PUBLIC_MEMPOOLSPACE_API_URL}${PUBLIC_MEMPOOLSPACE_ENDPOINT_LN_RANKING_CONNECTIVITY}`;
-  // TODO: error handling
-  const res = await fetch(url);
-  const nodes = await res.json();
-  return { nodes };
+// small utility function to have golang style error handling instead of try/catch blocks
+const catchError = (promise: Promise<any>) => {
+  return promise
+    .then((data) => {
+      return [null, data];
+    })
+    .catch((error) => {
+      return [error, null];
+    });
+};
+
+interface Node {
+  channels: Number;
+  alias: String;
+}
+interface NodesPageData {
+  nodes: Array<Node>;
+  errorTitle: null | String;
+  errorMessage: null | String;
+}
+export const load: PageLoad = async ({ fetch }): Promise<NodesPageData> => {
+  const url = `${PUBLIC_MEMPOOLSPACE_API_URL}${PUBLIC_MEMPOOLSPACE_ENDPOINT_LN_RANKING_CONNECTIVITY}`; // response ok and json
+  //const url = 'https://run.mocky.io/v3/048999b5-50b4-45fe-b277-cf03b852c17f'; // response ok and not json
+  //const url = "https://run.mocky.io/v3/01213725-e08e-447c-8472-f35d04a8ed84" // response not ok
+  // const url = 'https://run.sssssmocky.io/v3/01213725-e08e-447c-8472-f35d04a8ed84'; // failed to fetch
+
+  let errorTitle = null;
+  let errorMessage = null;
+
+  const [fetchError, res] = await catchError(fetch(url));
+  if (fetchError !== null) {
+    errorTitle = `Fetch Failed`;
+    errorMessage = fetchError.message;
+    console.error(fetchError);
+    return { errorTitle, errorMessage, nodes: [] };
+  }
+  if (res.ok === false) {
+    errorTitle = `API Error`;
+    errorMessage = `Response status ${res.status}`;
+    console.error(errorMessage);
+    console.error(res);
+    return { errorTitle, errorMessage, nodes: [] };
+  }
+
+  const [jsonError, nodes] = await catchError(res.json());
+  if (jsonError !== null) {
+    errorTitle = 'API Error';
+    errorMessage = 'API Error: Invalid JSON';
+    console.error(errorMessage);
+    console.error(jsonError);
+    return { errorTitle, errorMessage: 'Invalid JSON', nodes: [] };
+  }
+
+  return { errorTitle, errorMessage, nodes };
 };
