12194a3949f5523dab547f5c9e6902a61b7ea104
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-16 15:31:07 -0300

    feat: new home page
    Replaces the boilerplate default home page with a new one containing a
    basic navigation for upcomming app sections/features. Starting by the
    list visualization of popular LN nodes (`/nodes` route).

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Lightning Network Information</h1>
+<nav>
+  <ul>
+    <li><a href="./nodes">Popular Nodes</a></li>
+  </ul>
+</nav>
