b1da600e3cbc3ac31935157246e42b62b88238a0
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-16 15:21:17 -0300

    style: improve listing visuals
    This patch crafts a better node listing rendering with the help of
    Tailwind CSS.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,16 @@
-<h1>Lightning Network Information</h1>
-<nav>
-  <ul>
-    <li><a href="./nodes">Popular Nodes</a></li>
-  </ul>
-</nav>
+<div class="m-auto w-2/3 py-8 bg-slate-200 px-8 h-screen shadow-xl">
+  <h1 class="text-2xl my-8">Lightning Network Information</h1>
+  <nav>
+    <ul>
+      <li><a class="underline" href="./nodes">Popular Nodes</a></li>
+    </ul>
+  </nav>
+</div>
+
+<style lang="postcss">
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-amber-700);
+  }
+</style>
+
--- src/routes/nodes/+page.svelte
+++ src/routes/nodes/+page.svelte
@@ -4,28 +4,41 @@
   const { nodes } = data;
 </script>
 
-<article>
-  <header id="list-header">
-    <h1>Top Lightning Network Nodes</h1>
-    <p>Ordered by Connectivity <sup><a href="#fn1" aria-describedby="fn1">1</a></sup></p>
+<article class="w-full">
+  <header
+    class="bg-black px-8 py-4 text-white md:fixed md:top-0 md:right-0 md:left-0 md:h-20 md:shadow-xl"
+  >
+    <h1 class="flex justify-center text-lg">Top Lightning Network Nodes</h1>
+    <p class="flex justify-center text-sm/8">
+      Ordered by Connectivity <a href="#fn1" aria-describedby="fn1" class="underline">*</a>
+    </p>
   </header>
-  <ol>
+  <a id="list-header" class="md:h-30 md:block"/>
+  <ol class="bg-white md:m-auto md:max-w-2/3">
     {#each nodes as node}
-      <li>
-        <dd>{node.alias}</dd>
-        <dt>{node.channels}</dt>
+      <li class="flex justify-between border-b border-zinc-500 p-2">
+        <dd class="max-w-2/3 truncate">{node.alias}</dd>
+        <dt class="text-gray-600">{node.channels}</dt>
       </li>
     {/each}
   </ol>
-  <footer>
+  <footer class="bg-black text-sm text-white">
     <div id="footnotes">
       <ol>
         <li id="fn1">
-          source: <a href="https://mempool.space/lightning/nodes/rankings/connectivity"
+          source: <a class="underline" href="https://mempool.space/lightning/nodes/rankings/connectivity"
             >mempool.space</a
-          ><a href="#list-header" aria-label="Back to reference">↩</a>
+          > <a href="#list-header" aria-label="Back to reference">↩</a>
         </li>
       </ol>
     </div>
   </footer>
 </article>
+
+<style lang="postcss">
+  /* required to use tailwind inside style tags */
+  @reference "tailwindcss";
+  :global(html) {
+    background-color: theme(--color-slate-600);
+  }
+</style>
