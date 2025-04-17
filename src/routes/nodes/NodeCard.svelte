2d9d0633708013b800b32c61d6d6d31bc7d3c364
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-17 12:04:55 -0300

    test: initial unit tests for the LN node card component
    Vitest tests using testing-library and jest-dom to check expected
    rendering texts and aria roles.

--- src/routes/nodes/NodeCard.svelte
+++ src/routes/nodes/NodeCard.svelte
@@ -2,12 +2,16 @@
   let { alias, channels, publicKey } = $props();
 </script>
 
-<div class="group flex flex-col content-center gap-2 border-b border-zinc-500 p-2">
+<div
+  role="group"
+  aria-describedby={`alias-${publicKey}`}
+  class="group flex flex-col content-center gap-2 border-b border-zinc-500 p-2"
+>
   <div class="flex flex-row justify-between">
-    <dt class="truncate">{alias}</dt>
-    <dd class="text-gray-600">{channels}</dd>
+    <h1 id={`alias-${publicKey}`} class="truncate">{alias}</h1>
+    <div class="text-gray-600">{channels}</div>
   </div>
-  <dd class="text-align-right grow truncate text-left align-baseline text-xs text-slate-500">
+  <h2 class="text-align-right grow truncate text-left align-baseline text-xs text-slate-500">
     {publicKey}
-  </dd>
+  </h2>
 </div>
