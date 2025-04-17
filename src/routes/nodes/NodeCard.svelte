32669912b2deb95b30ebc471cbc065aede2b8d5b
Fabricio C Zuardi <fabricio@fabricio.org>
2025-04-17 18:04:56 -0300

    feat: add remaining fields
    - firstSeenAt and updatedAt with the timestamp converted to something
    better than unix timestamps, with tooltip in iso.
    - country when available
    - use details tag to have expandable card

--- src/routes/nodes/NodeCard.svelte
+++ src/routes/nodes/NodeCard.svelte
@@ -7,17 +7,29 @@
     const decimalPlaces = 8;
     return (capacityInSats / satoshisInOneBtc).toFixed(decimalPlaces);
   }
+  // TODO: take into consideration i18n
+  // TODO: or even better, use a flag emoji
+  function displayCountry(countryObject, language) {
+    return countryObject?.[language] || 'not informed';
+  }
+  function displayDate(unixtime) {
+    return new Date(unixtime * 1000).toLocaleDateString();
+  }
+  function displayDateTooltip(unixtime) {
+    return new Date(unixtime * 1000).toISOString();
+  }
 </script>
 
-<div
-  role="group"
+<details
   aria-describedby={`alias-${publicKey}`}
   class="group flex flex-col content-center gap-2 border-b border-zinc-500 p-2"
 >
-  <div class="flex flex-row justify-between">
-    <h1 id={`alias-${publicKey}`} title="alias" class="truncate">{alias}</h1>
+  <summary class="flex flex-row justify-between">
+    <h1 id={`alias-${publicKey}`} title="alias" class="grow truncate">{alias}</h1>
+    <p class="mr-4 text-zinc-400 opacity-0 group-hover:opacity-100">click me</p>
     <div class="text-gray-600" title="channels">{channels}</div>
-  </div>
+  </summary>
+
   <div class="flex">
     <h2
       title="public key"
@@ -27,4 +39,20 @@
     </h2>
     <div class="text-sm text-gray-600" title="capacity">{displayCapacity(capacity)} BTC</div>
   </div>
-</div>
+  <table class="mt-2 mb-2 w-full">
+    <thead class="abhhg-zinc-300 p-2 text-sm font-bold text-zinc-400">
+      <tr class="p-2">
+        <td>First Seen</td>
+        <td>Updated At</td>
+        <td>Country</td>
+      </tr>
+    </thead>
+    <tbody class="text-xs">
+      <tr>
+        <td title={displayDateTooltip(firstSeen)}>{displayDate(firstSeen)}</td>
+        <td title={displayDateTooltip(updatedAt)}>{displayDate(updatedAt)}</td>
+        <td>{displayCountry(country, 'en')}</td>
+      </tr>
+    </tbody>
+  </table>
+</details>
